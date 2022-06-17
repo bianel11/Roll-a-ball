ee390b3f1a39fb882b493514fb4aa83682be5e83
Bianel <bianel9815@gmail.com>
2022-06-17 19:37:51 -0400

    apply last changes

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,55 +5,55 @@ using TMPro;
 using UnityEngine.UI;
 public class PlayerController : MonoBehaviour
 {
-    private Rigidbody player;
+  private Rigidbody player;
 
-    public int points;
-    public float playerVelocity;
+  public int points;
+  public float playerVelocity;
 
-    public TMPro.TextMeshProUGUI textoContador, textoGanar;
+  public TMPro.TextMeshProUGUI textoContador, textoGanar;
 
-    // Start is called before the first frame update
-    void Start()
+  // Start is called before the first frame update
+  void Start()
+  {
+    player = GetComponent<Rigidbody>();
+    points = 0;
+    setTextoContador();
+    textoGanar.text = "";
+  }
+
+  void FixedUpdate()
+  {
+    // Movements vars
+    float movH = Input.GetAxis("Horizontal");
+    float movV = Input.GetAxis("Vertical");
+
+    Vector3 movements = new Vector3(movH, 0.0f, movV);
+    player.AddForce(movements * playerVelocity);
+  }
+
+  // Update is called once per frame
+  void Update()
+  {
+
+  }
+
+  //Se ejecuta al entrar a un objeto con la opción isTrigger seleccionada
+  void OnTriggerEnter(Collider other)
+  {
+    if (other.gameObject.CompareTag("Coleccionable"))
     {
-        player = GetComponent<Rigidbody>();
-        points = 0;
-        setTextoContador();
-        textoGanar.text = "";	
+      other.gameObject.SetActive(false);
+      points++;
+      setTextoContador();
     }
+  }
 
-    void FixedUpdate()
+  void setTextoContador()
+  {
+    textoContador.text = "Contador:" + points.ToString();
+    if (points >= 15)
     {
-        // Movements vars
-        float movH = Input.GetAxis("Horizontal");
-        float movV = Input.GetAxis("Vertical");
-
-        Vector3 movements = new Vector3(movH, 0.0f, movV);
-        player.AddForce(movements * playerVelocity);
+      textoGanar.text = "Ganaste!";
     }
-
-    // Update is called once per frame
-    void Update()
-    {
-
-    }
-
-    //Se ejecuta al entrar a un objeto con la opción isTrigger seleccionada
-    void OnTriggerEnter(Collider other)
-    {
-        if (other.gameObject.CompareTag("Coleccionable"))
-        {
-            other.gameObject.SetActive(false);
-            points++;
-            setTextoContador();
-        }
-    }
-
-    void setTextoContador()
-    {
-        textoContador.text = "Contador:" + points.ToString();
-        if (points >= 4)
-        {
-            textoGanar.text = "Ganaste!";
-        }
-    }
+  }
 }
